ae1437ce6067afc28258dcdbdaebfb06c8d401d3
gianriyanto <li.griyanto@gmail.com>
2021-02-06 12:09:39 +1100

    Setup GetStarted.vue and Testimonies.vue section

---
+++ src/components/GetStarted.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="GetStarted">
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "GetStarted"
+}
+</script>
+
+<style lang="scss" scoped>
+
+@media only screen and (min-width: 769px) {
+
+  #GetStarted{
+    border: thin solid lightskyblue;
+    height: 100vh;
+  }
+}
+
+</style>
