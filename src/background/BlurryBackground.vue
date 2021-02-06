2f13113f5a9dad75cd7b59acb658b617fa104f38
gianriyanto <li.griyanto@gmail.com>
2021-02-06 12:38:52 +1100

    Removed box shadown in BlurryBackground.vue

--- src/background/BlurryBackground.vue
+++ src/background/BlurryBackground.vue
@@ -36,7 +36,6 @@ export default {
       height: 100%;
       z-index: 1;
       background: rgba( 255, 255, 255, 0.6 );
-      box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
       backdrop-filter: blur( 70.0px );
       -webkit-backdrop-filter: blur( 70.0px );
       border-radius: 10px;
